67206864e6f653300ad675e4082eb749921c243e
unknown <jontopanotti@gmail.com>
2024-08-13 14:21:13 -0300

    init

---
+++ crates/rune/src/lib.rs
@@ -0,0 +1,28 @@
+mod expr;
+mod binding_def;
+mod val;
+mod stmt;
+mod utils;
+mod env;
+
+pub use env::Env;
+pub use val::Val;
+
+#[derive(Debug)]
+pub struct Parse(stmt::Stmt);
+
+impl Parse {
+    pub fn eval(&self, env: &mut Env) -> Result<Val, String> {
+        self.0.eval(env)
+    }
+}
+
+pub fn parse(s: &str) -> Result<Parse, String> {
+    let (s, stmt) = stmt::Stmt::new(s)?;
+
+    if s.is_empty() {
+        Ok(Parse(stmt))
+    } else {
+        Err("input was not consumed fully".to_string())
+    }
+}
